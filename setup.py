cb2b0e134d4b8aa095fd650dd609dbf37aeedf21
xingweitian <13183370+xingweitian@users.noreply.github.com>
2019-06-19 14:20:37 -0400

    Reconstruct the code, add setup.py, make it running in terminal as a command.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FundMyWatCard",
+    version="0.1",
+    packages=find_packages(),
+    scripts=['fundmywatcard/watcard'],
+    install_requires=['EasyProcess==0.2.7',
+                      'PyVirtualDisplay==0.2.3',
+                      'selenium==3.141.0',
+                      'six==1.12.0',
+                      'splinter==0.10.0',
+                      'urllib3==1.25.3'
+                      ],
+    author="xingweitian",
+    author_email="xingweitian@gmail.com",
+    description="A tool to fund your WatCard in an easy way",
+    license="GPL-3.0",
+    keywords="WatCard",
+    url="https://github.com/xingweitian/fund-my-watcard"
+)
