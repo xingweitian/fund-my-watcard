4ed4860482577e60e2fb88a375d9095e9958eadf
xingweitian <13183370+xingweitian@users.noreply.github.com>
2019-06-24 23:57:29 -0400

    Fix bug.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,9 +1,7 @@
-import json
+from fund_my_watcard.config import VERSION
 from .util import out
 
 
 def test_print_version():
-    with open("fund-my-watcard/config.json") as f:
-        config = json.load(f)
     _version = out("watcard --version")
-    assert _version.strip() == "fund-my-watcard v{}".format(config["VERSION"])
+    assert _version.strip() == "fund-my-watcard v{}".format(VERSION)
