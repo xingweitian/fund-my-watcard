d1347fbc1a23d15705101934a936e51ff5baad21
xingweitian <13183370+xingweitian@users.noreply.github.com>
2019-06-24 23:55:16 -0400

    Fix bug.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-import json
-
 from setuptools import setup, find_packages
+
+from fund_my_watcard.config import VERSION
 
 with open("requirements.txt") as f:
     requirements = [_.strip() for _ in f.readlines() if _]
@@ -8,16 +8,12 @@ with open("requirements.txt") as f:
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
-with open("fund-my-watcard/config.json") as f:
-    config = json.load(f)
-
 setup(
     name="fund-my-watcard",
-    version=config["VERSION"],
-    packages=find_packages(exclude=["tests"]),
+    version=VERSION,
+    packages=find_packages("fund_my_watcard"),
     python_requires=">=3.5",
-    scripts=["fund-my-watcard/watcard"],
-    include_package_data=True,
+    scripts=["fund_my_watcard/watcard"],
     install_requires=requirements,
     author="xingweitian",
     author_email="xingweitian@gmail.com",
