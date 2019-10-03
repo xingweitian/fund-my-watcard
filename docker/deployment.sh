69e3d78c686e781b061bc85f154facfe9a024d80
xingweitian <13183370+xingweitian@users.noreply.github.com>
2019-10-02 21:37:23 -0400

    Improve shell script.

--- docker/deployment.sh
+++ docker/deployment.sh
@@ -5,8 +5,8 @@ set -e
 VERSION=$1
 USERNAME="faushine"
 
-docker build --no-cache -t $USERNAME/fund-my-watcard:$VERSION .
+docker build --no-cache -t $USERNAME/fund-my-watcard:"$VERSION" .
 
-docker push $USERNAME/fund-my-watcard:$VERSION
+docker push $USERNAME/fund-my-watcard:"$VERSION"
 
 exit
